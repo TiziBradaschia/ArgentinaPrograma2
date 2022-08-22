b1ace5bc1acb590d0d11cc5dc2e6bb91f0fb3d84
TiziBradaschia <tizibradaschia@gmail.com>
2022-08-22 19:47:44 -0300

    Footer

--- tiziana/FrontEnd/src/app/app.module.ts
+++ tiziana/FrontEnd/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { EstudiosComponent } from './components/estudios/estudios.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { ProyectosComponent } from './components/proyectos/proyectos.component';
     ExperienciaComponent,
     EstudiosComponent,
     SkillsComponent,
-    ProyectosComponent
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
